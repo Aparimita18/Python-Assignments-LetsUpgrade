a0502a7f81e078f7e652c061ca3913df4ad14f44
Aparimita Ranpise <118061971+Aparimita18@users.noreply.github.com>
2023-03-05 14:41:16 +0530

    Add files via upload

---
+++ 18 jan.py
@@ -0,0 +1,12 @@
+s = 'Maheshwar Sir'
+
+# split the string into a list of words
+words = s.split()
+
+# reverse the list of words
+words.reverse()
+
+# join the reversed list of words into a string
+reversed_string = ' '.join(words)
+
+print(reversed_string)
